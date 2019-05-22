8fec8496386b4264116de42625b5017b02eac704
Matheus Lessa <matheus.220@hotmail.com>
2019-05-22 09:51:21 +0200

    remove files

--- scripts/schemas.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import *
-
-class Cycle(DynamicDocument):
-    cycle_id = FloatField()
-    start_time = StringField()
-    end_time = StringField()
-    mode = StringField()
-    status = StringField()
